13f89a15ecbd459ab79c71d55ee65974622a3406
rick237518 <67753269+rick237518@users.noreply.github.com>
2020-11-18 19:38:50 +0100

    Updates

---
+++ precache-manifest.5ecbe7c3672b7dd267c975c4661003cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4634da79ca48494a57e074466a41b7aa",
+    "url": "/rolodex-api/index.html"
+  },
+  {
+    "revision": "f83571c4cf9056402d34",
+    "url": "/rolodex-api/static/css/main.13195a95.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/rolodex-api/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex-api/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83571c4cf9056402d34",
+    "url": "/rolodex-api/static/js/main.5a54f1ae.chunk.js"
+  },
+  {
+    "revision": "87d3bc4b6f804431179d",
+    "url": "/rolodex-api/static/js/runtime-main.aa79281c.js"
+  }
+]);
